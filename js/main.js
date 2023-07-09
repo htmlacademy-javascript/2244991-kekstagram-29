9f4843dfb48af2eef1634aeb4f40e57a10c69689
Taisiya Bogatyreva <bogatyrevata@gmail.com>
2023-07-09 00:59:10 -0300

    добавила импорты

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { generatePhotos } from './mocks/data.js';
 import { renderTumbnails } from './thumbnail.js';
+import { renderGallery } from './gallery.js';
 
 const PICTURES_COUNT = 25;
 
 const photosData = generatePhotos(PICTURES_COUNT);// Генерируем массив из 25 фотографий с помощью функции генерации массива
 renderTumbnails(photosData);//генерируем миниатюры фотографий
+renderGallery(photosData);//генерируем галерею фотографий
